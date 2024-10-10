6e2f59962ea5eda83be3e0a9290d8ed9be032c38
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-11 06:40:58 +0800

    update: add login page foundation form

--- src/views/student/login.php
+++ src/views/student/login.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Student Login</title>
+  <!-- Design by foolishdeveloper.com -->
+    <title>Glassmorphism login Form Tutorial in html css</title>
+ 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/cssc/public/assets/css/global.css">
+    <link rel="stylesheet" href="/cssc/public/assets/css/auth.css">
+    <!--Stylesheet-->
 </head>
 <body>
-    <h1>Student Login Page</h1>
+    <form>
+        <h3>Student Account</h3>
+        <label for="username">Username</label>
+        <input type="text" id="username">
+        <div class="password">
+            <label for="password">Password</label>
+            <a href="#" class="forgot-password">forgot password?</a>
+        </div>
+        <input type="password" id="password">
+        <button type="submit" class="submit-button">log in</button>
+    </form>
 </body>
 </html>
