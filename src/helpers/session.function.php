f079cc17f23ad194f9fdec47971cd807e0076bf8
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-12 15:46:57 +0800

    modified: set session start to conditional to prevent multiple sessions attempt

--- src/helpers/session.function.php
+++ src/helpers/session.function.php
@@ -1,7 +1,10 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-session_start();
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
 
 function isLoggedIn() {
