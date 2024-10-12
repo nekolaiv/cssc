707e0ef5f67c255a709284cd44906a85c1c15c83
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-12 20:35:05 +0800

    update: sql file

--- sql/cssc.sql
+++ sql/cssc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Oct 10, 2024 at 03:33 AM
+-- Generation Time: Oct 12, 2024 at 02:34 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Admin`
+--
+
+CREATE TABLE `Admin` (
+  `admin_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Awards`
 --
 
@@ -31,20 +46,8 @@ CREATE TABLE `Awards` (
   `award_id` int(11) NOT NULL,
   `award_name` varchar(100) NOT NULL,
   `min_gpa` decimal(5,2) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Courses`
---
-
-CREATE TABLE `Courses` (
-  `course_id` int(11) NOT NULL,
-  `course_name` varchar(100) NOT NULL,
-  `credits` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -59,21 +62,59 @@ CREATE TABLE `Grades` (
   `course_id` int(11) NOT NULL,
   `grade` decimal(3,2) NOT NULL,
   `verification_status` enum('PENDING','VERIFIED','REJECTED') DEFAULT 'PENDING',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- Table structure for table `Staffs`
 --
 
-CREATE TABLE `Users` (
-  `user_id` int(11) NOT NULL,
+CREATE TABLE `Staffs` (
+  `staff_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('ADMIN','INSTRUCTOR','STUDENT') NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Students`
+--
+
+CREATE TABLE `Students` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `Students`
+--
+
+INSERT INTO `Students` (`user_id`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(3, 'student1@wmsu.edu.ph', 'student1123', '2024-10-12 12:11:40', '2024-10-12 12:11:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Subjects`
+--
+
+CREATE TABLE `Subjects` (
+  `course_id` int(11) NOT NULL,
+  `subject_name` varchar(100) NOT NULL,
+  `subject_code` varchar(100) NOT NULL,
+  `credits` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -107,16 +148,18 @@ CREATE TABLE `User_Shifting` (
 --
 
 --
+-- Indexes for table `Admin`
+--
+ALTER TABLE `Admin`
+  ADD PRIMARY KEY (`admin_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- Indexes for table `Awards`
 --
 ALTER TABLE `Awards`
   ADD PRIMARY KEY (`award_id`);
-
---
--- Indexes for table `Courses`
---
-ALTER TABLE `Courses`
-  ADD PRIMARY KEY (`course_id`);
 
 --
 -- Indexes for table `Grades`
@@ -127,11 +170,25 @@ ALTER TABLE `Grades`
   ADD KEY `course_id` (`course_id`);
 
 --
--- Indexes for table `Users`
+-- Indexes for table `Staffs`
 --
-ALTER TABLE `Users`
+ALTER TABLE `Staffs`
+  ADD PRIMARY KEY (`staff_id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `Students`
+--
+ALTER TABLE `Students`
   ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indexes for table `Subjects`
+--
+ALTER TABLE `Subjects`
+  ADD PRIMARY KEY (`course_id`);
 
 --
 -- Indexes for table `User_Awards`
@@ -153,16 +210,16 @@ ALTER TABLE `User_Shifting`
 --
 
 --
+-- AUTO_INCREMENT for table `Admin`
+--
+ALTER TABLE `Admin`
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `Awards`
 --
 ALTER TABLE `Awards`
   MODIFY `award_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Courses`
---
-ALTER TABLE `Courses`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `Grades`
@@ -171,10 +228,22 @@ ALTER TABLE `Grades`
   MODIFY `grade_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `Users`
+-- AUTO_INCREMENT for table `Staffs`
 --
-ALTER TABLE `Users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `Staffs`
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Students`
+--
+ALTER TABLE `Students`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `Subjects`
+--
+ALTER TABLE `Subjects`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `User_Awards`
@@ -196,21 +265,21 @@ ALTER TABLE `User_Shifting`
 -- Constraints for table `Grades`
 --
 ALTER TABLE `Grades`
-  ADD CONSTRAINT `Grades_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
-  ADD CONSTRAINT `Grades_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `Courses` (`course_id`);
+  ADD CONSTRAINT `Grades_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Students` (`user_id`),
+  ADD CONSTRAINT `Grades_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `Subjects` (`course_id`);
 
 --
 -- Constraints for table `User_Awards`
 --
 ALTER TABLE `User_Awards`
-  ADD CONSTRAINT `User_Awards_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`),
+  ADD CONSTRAINT `User_Awards_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Students` (`user_id`),
   ADD CONSTRAINT `User_Awards_ibfk_2` FOREIGN KEY (`award_id`) REFERENCES `Awards` (`award_id`);
 
 --
 -- Constraints for table `User_Shifting`
 --
 ALTER TABLE `User_Shifting`
-  ADD CONSTRAINT `User_Shifting_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`);
+  ADD CONSTRAINT `User_Shifting_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Students` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
