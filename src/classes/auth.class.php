46d339ffb1404f822622dfd8cb24918905e11831
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-12 15:46:08 +0800

    modified: set session start ot conditional to prevent multiple sessions attempt

--- src/classes/auth.class.php
+++ src/classes/auth.class.php
@@ -1,8 +1,10 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-session_start();
 require_once "database.class.php";
 
 class Auth{
@@ -32,7 +34,6 @@ class Auth{
 			header("location:" . $_SERVER["HTTP_REFERER"]);
 			return;
 		}
-
 		// if (password_verify($_POST["password"], $student["password"])) 
 		if ($_POST["password"] == $student["password"]){
 			$_SESSION["user_id"] = $student["user_id"];
@@ -40,16 +41,26 @@ class Auth{
 			$_SESSION["email"] = $student["email"];
 			$_SESSION["user_type"] = "student";
 			header("location: " . $_SERVER["HTTP_REFERER"]);
-		}  
+		} else {
+			return False;
+		}
   	}
 
-	function studentResetPassword(){
-		extract($_POST);
-
-		$sql = "UPDATE Student set password = :password WHERE email = :email";
+	function studentResetPassword($email, $new_password){
+		$sql = "UPDATE Students set password = :password WHERE email = :email";
 		$query = $this->database->connect()->prepare($sql);
-		$query->bindParam(':email', $_POST['email']);
+		$query->bindParam(':email', $email);
+		$query->bindParam(':password', $new_password);
 		return $query->execute();
+	}
+
+	function emailExists($email){
+		$sql = "SELECT COUNT(*) FROM Students WHERE email = :email";
+		$query = $this->database->connect()->prepare($sql);
+		$query->bindParam(':email', $email);
+		$query->execute();
+		$count = $query->fetchColumn();
+		return $count > 0;
 	}
 
 
