74c2e6558a58c4cc9401f9003a8f3fc4e3ce10d2
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-12 16:09:55 +0800

    add: meta tags

--- src/views/student/forgot-password.php
+++ src/views/student/forgot-password.php
@@ -36,6 +36,8 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Login Form</title>
     <link rel="stylesheet" href="/cssc/public/assets/css/global.css">
     <link rel="stylesheet" href="/cssc/public/assets/css/auth.css">
--- src/views/student/register.php
+++ src/views/student/register.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Student Login Form</title>
     <link rel="stylesheet" href="/cssc/public/assets/css/global.css">
     <link rel="stylesheet" href="/cssc/public/assets/css/auth.css">
