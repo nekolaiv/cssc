5f69209dba30414cbddbd53a36f534e478f59f49
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-18 13:51:06 +0800

    update: set auth for admin

--- src/classes/auth.class.php
+++ src/classes/auth.class.php
@@ -168,6 +168,82 @@ class Auth{
 		return $count > 0;
 	}
 
+	// ADMIN SECTION
+
+	function adminLogin($email, $password) {
+		$sql = "SELECT * FROM Staffs WHERE email = :email";
+		$query = $this->database->connect()->prepare($sql);
+		$query->bindParam(':email', $email);
+		$admin=null;
+		if($query->execute()){
+			$admin = $query->fetch();
+		}
+
+		if (!$admin) {
+			header("location:" . $_SERVER["HTTP_REFERER"]);
+			return;
+		}
+		if (password_verify($password, $admin["password"])){
+			$_SESSION["user_id"] = $admin["user_id"];
+			$_SESSION["is_loggedIn"] = TRUE;
+			$_SESSION["email"] = $admin["email"];
+			$_SESSION["user_type"] = "admin";
+			header("location: " . $_SERVER["HTTP_REFERER"]);
+		} else {
+			return False;
+		}
+  	}
+
+	function adminRegister(string $email, string $password){
+		$sql = "INSERT INTO Admin(email, password) VALUES(:email, :password)";
+		$query = $this->database->connect()->prepare($sql);
+		$query->bindParam(':email', $email);
+		$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+		$query->bindParam(':password', $hashed_password);
+		if($query->execute()){
+            return true;
+        } else {
+            return false;
+        }
+	}
+
+	private function _retrieveAdminPassword($email, $password){
+		$sql = "SELECT password FROM Admin WHERE email = :email";
+		$query = $this->database->connect()->prepare($sql);
+		$query->bindParam(':email', $email);
+		$retrieved_password=null;
+		if($query->execute()){
+			$retrieved_password = $query->fetch();
+		}
+		if(password_verify($password, $retrieved_password["password"])){
+			return true;
+		}
+		return false;
+	}
+
+	function adminResetPassword($email, $new_password){
+		$sql = "UPDATE Admin set password = :password WHERE email = :email";
+		$new_hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+		if(!$this->_retrieveStPassword($email, $new_password)){
+			$query = $this->database->connect()->prepare($sql);
+			$query->bindParam(':email', $email);
+			$query->bindParam(':password', $new_hashed_password);
+			return $query->execute();
+		} else {
+			return false;
+		}
+		
+	}
+
+	function adminEmailExists($email){
+		$sql = "SELECT COUNT(*) FROM Admin WHERE email = :email";
+		$query = $this->database->connect()->prepare($sql);
+		$query->bindParam(':email', $email);
+		$query->execute();
+		$count = $query->fetchColumn();
+		return $count > 0;
+	}
+
 
 }
 
