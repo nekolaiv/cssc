2396a599278d7062df67c3ceeee2d44f8a4f5444
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-10-27 05:45:09 +0800

    update: refactor for login

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+$rootDir = dirname(__FILE__, 3);
+
+define('FRONT_DIR', '/cssc/public/index.php');
+define('ROOT_DIR', '/cssc/');
+define('STUDENT_DIR', '/cssc/resources/views/student/');
+define('STAFF_DIR', '/cssc/resources/views/staff/');
+define('ADMIN_DIR', '/cssc/resources/views/admin/');
+
+?>
