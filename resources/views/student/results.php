c18a10e8c411f62ed1059b04269f2bf10a68a2cb
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-11-21 08:14:32 +0800

    update: render status from session profile and temp add button for submitting entries

--- resources/views/student/results.php
+++ resources/views/student/results.php
@@ -4,10 +4,11 @@
     <h2 id="result-message-3"><?php echo $_SESSION['GWA']['message-3']?></h2>
     <h2 id="result-message-4">GWA SCORE: <?php echo $_SESSION['GWA']['gwa-score']?></h2>
     <h2 id="result-verification-status">Verification Status: 
-        <?php isset($_SESSION['result-verification-status']) ? $status = $_SESSION['result-verification-status'] : $status = "Pending"; echo $status;?>
+        <?php echo $_SESSION['profile']['status'];?>
     </h2>
     <div id="result-action-buttons">
         <button class="nav-items" onclick="loadPage('home.php')">Back to Home</button>
+        <button class="nav-items" onclick="loadPage('calculate.php')">Edit Inputs</button>
         <button class="nav-items" onclick="loadPage('calculate.php')">Edit Inputs</button>
     </div>
 </div>
