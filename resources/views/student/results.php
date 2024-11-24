fbd5737c92d5b2aabc3a3545c59f3d731c84f780
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-11-24 13:16:07 +0800

    add validate button

--- resources/views/student/results.php
+++ resources/views/student/results.php
@@ -9,6 +9,6 @@
     <div id="result-action-buttons">
         <button class="nav-items" onclick="loadPage('home.php')">Back to Home</button>
         <button class="nav-items" onclick="loadPage('calculate.php')">Edit Inputs</button>
-        <button class="nav-items" onclick="loadPage('calculate.php')">Edit Inputs</button>
+        <button type="submit" class="nav-items" id="submit-calculation-entry"> <?php echo $validate_button ?? "Validate Entry 1" ?></button>
     </div>
 </div>
