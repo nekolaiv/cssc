9247baf37f0e2879196366e32f7a540a48ba30a1
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-11-30 13:00:45 +0800

    update: add clean numeric input

--- tools/clean.function.php
+++ tools/clean.function.php
@@ -5,4 +5,20 @@ function cleanInput($input){
     $input = htmlspecialchars($input);
     return $input;
 }
+
+function cleanNumericInput($input) {
+        if ($input === '' || $input === NULL) {
+            return false;
+        } else { 
+            $input = trim($input);
+            $input = stripslashes($input);
+            $input = htmlspecialchars($input);
+
+            if (is_numeric($input)) {
+                return $input;
+            } else {
+                return false;
+            }
+        }
+    }
 ?>
