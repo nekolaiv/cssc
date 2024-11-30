0d40aae3e115a301e57e7792f2df86296de91be5
Nekolaiv <177994716+nekolaiv@users.noreply.github.com>
2024-11-30 13:01:08 +0800

    update

--- views/student/calculate.php
+++ views/student/calculate.php
@@ -1,22 +1,3 @@
-<?php 
-$page_title = "calculate";
-include_once "../../includes/_student-head.php";
+<?php
+require_once('./contents/calculate.php');
 ?>
-
-<body class="home-body">
-    <main class="wrapper">
-        <?php include_once "../../includes/_student-header.php"?>
-        <div class="content">
-            <!-- content dynamic load -->
-        </div>
-    </main>
-
-<!-- <script>
-    let session_length = <?php //count($_SESSION["course-fields"]["subject_code"] ?? []) ?>
-</script> -->
-<!-- <script src="/csrs/js/student_ajax.js"></script> -->
-<script src="/cssc/controllers/student-controller.js"></script>
-<?php include_once "../../includes/_student-footer.php"?>
-
-
-
